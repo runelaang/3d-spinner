7a297bbbc1eacda1d9b8422f57b048a33d9627f1
Rune L <rune@medius.one>
2026-07-05 12:39:34 +0200

    Refactor car mesh loading in examples to utilize MTL file for material colors. Update parseObj function to support MTL colors and add tests for MTL color application.

---
+++ tests/obj-loader.test.mjs
@@ -0,0 +1,28 @@
+import { test } from "node:test";
+import assert from "node:assert/strict";
+import { parseObj } from "../dist/engines/little-3d-engine/loaders/obj.js";
+
+const obj = `
+v 0 0 0
+v 1 0 0
+v 0 1 0
+usemtl Painted
+f 1 2 3
+usemtl Missing
+f 1 3 2
+`;
+
+const mtl = `
+newmtl Painted
+Kd 0.1 0.5 1.0
+`;
+
+test("parseObj uses MTL Kd colors when enabled", () => {
+  const mesh = parseObj(obj, { colors: ["#abcdef"], mtl, useMtlColors: true });
+  assert.deepEqual(mesh.faces.map((face) => face.color), ["#1a80ff", "#abcdef"]);
+});
+
+test("parseObj ignores MTL colors by default", () => {
+  const mesh = parseObj(obj, { colors: ["#abcdef"], mtl });
+  assert.deepEqual(mesh.faces.map((face) => face.color), ["#abcdef", "#abcdef"]);
+});
